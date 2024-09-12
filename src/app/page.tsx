04eedf215da2edb234214cacea6922738e0e631f
Sam Frons <samfrons@gmail.com>
2024-09-12 17:21:04 -0400

    new deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,6 @@ import React from 'react';
 import dynamic from 'next/dynamic';
 
 
-//import Mfc1 from './components/Mfc1';
-
 const SocialMediaVisualization = dynamic(
   () => import('./components/SocialMediaVisualization'),
   { ssr: false }

f74a72d631e6b69f77e5128f56eb53779676b0ad
Sam Frons <samfrons@gmail.com>
2024-09-16 21:57:37 -0400

    fonts

--- src/app/components/DisasterTypeChart.tsx
+++ src/app/components/DisasterTypeChart.tsx
@@ -1,16 +1,20 @@
 import React, { useMemo } from 'react';
-import { Chart as ChartJS, CategoryScale, LinearScale, LineElement, Title, Tooltip, Legend, Filler } from 'chart.js';
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend, Filler } from 'chart.js';
 import { Line } from 'react-chartjs-2';
 
 ChartJS.register(
   CategoryScale,
   LinearScale,
+  PointElement,
   LineElement,
   Title,
   Tooltip,
   Legend,
   Filler
 );
+
+
+
 
 interface Movie {
   title: string;
